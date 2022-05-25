b706244080a1644eab79006a90f87edc2c482edd
Vladimi <ovladimir848@gmail.com>
2022-05-25 17:36:41 +0300

    Статика (2)

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -115,10 +115,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join('../', BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join('../', BASE_DIR, 'media')
 
 
 REST_FRAMEWORK = {
